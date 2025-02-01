15257f6f700d30a664abd8e06e9694c80013c861
zhangdingpeng <zhangdingpeng@hdu.edu.cn>
2025-02-01 15:25:14 +0800

    启用禁用员工账号功能实现

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -35,4 +35,10 @@ public interface EmployeeMapper {
      * @return
      */
     Page<Employee> pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
+
+    /**
+     * 启用/禁用员工账号
+     * @param employee
+     */
+    void update(Employee employee);
 }
