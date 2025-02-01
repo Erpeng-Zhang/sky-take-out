80edf13528d79ff1ffc6444625144c51aa71f9b5
zhangdingpeng <zhangdingpeng@hdu.edu.cn>
2025-02-01 10:53:28 +0800

    新增员工功能改进，用户名重复提醒+操作人ID获取

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -1,20 +1,49 @@
 package com.sky.service.impl;
 
+//import com.sky.constant.MessageConstant;
+//import com.sky.constant.PasswordConstant;
+//import com.sky.constant.StatusConstant;
+//import com.sky.dto.EmployeeDTO;
+//import com.sky.dto.EmployeeLoginDTO;
+//import com.sky.entity.Employee;
+//import com.sky.exception.AccountLockedException;
+//import com.sky.exception.AccountNotFoundException;
+//import com.sky.exception.PasswordErrorException;
+//import com.sky.mapper.EmployeeMapper;
+//import com.sky.service.EmployeeService;
+//import org.springframework.beans.BeanUtils;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//import org.springframework.util.DigestUtils;
+
+
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.sky.constant.MessageConstant;
 import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
+import com.sky.context.BaseContext;
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import com.sky.exception.AccountLockedException;
 import com.sky.exception.AccountNotFoundException;
 import com.sky.exception.PasswordErrorException;
 import com.sky.mapper.EmployeeMapper;
+import com.sky.result.PageResult;
 import com.sky.service.EmployeeService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+
+
+
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
@@ -76,13 +105,13 @@ public class EmployeeServiceImpl implements EmployeeService {
         employee.setPassword(DigestUtils.md5DigestAsHex(PasswordConstant.DEFAULT_PASSWORD.getBytes()));
 
         //设置当前记录创建时间和修改时间
-        employee.setCreateTime(localDateTime.now());
-        employee.setUpdateTime(localDateTime.now());
+        employee.setCreateTime(LocalDateTime.now());
+        employee.setUpdateTime(LocalDateTime.now());
 
         //设置当前记录创建人和修改人id
-        // TODO: 后期改为当前登录用户id
-        employee.setCreateUser(10L);
-        employee.setUpdateUser(10L);
+        // 利用每次请求归属于同一个线程使用ThreadLocal存储用户ID
+        employee.setCreateUser(BaseContext.getCurrentId());
+        employee.setUpdateUser(BaseContext.getCurrentId());
 
         employeeMapper.insert(employee);
 
